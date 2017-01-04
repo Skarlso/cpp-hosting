3b089e885f4840f6557d11a530f64465caf2cc21
Gergely Brautigam <skarlso777@gmail.com>
2017-01-04 15:23:31 +0100

    Progress.

--- hosting/hosting.cpp
+++ hosting/hosting.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include "actions.h"
 #include "create_command.h"
+#include "command.h"
+#include <map>
+#include <string>
+
+// C++ Noob Note: Because * comes after the type. This is a pointer type value
+// And not * Command. Command is the type.
+std::map<std::string, Command *> commands;
 
 int main(int argv, char* args[]) {
     Actions *action = new Actions;
 
     CreateCommand *cc = new CreateCommand(action);
-    cc->execute();
+    commands["create"] = cc;
+    return 0;
 }
