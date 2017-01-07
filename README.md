00b518e2e6e555566d31e41d369530759220aae2
Gergely Brautigam <skarlso777@gmail.com>
2017-01-07 15:00:05 +0100

    Finally a working test case with working source.

--- README.md
+++ README.md
@@ -4,9 +4,23 @@
 
 Thanks to cmake's out-of-source builds, this way, every build information will be under
 the build directory and make will also work from there.
-```
+
+```bash
 $ git submodule init
 $ git submodule update
-$ cd build && cmake ..
-$ make
+$ cd build && cmake .. && cmake --build .
+```
+
+# Testing
+
+```bash
+$ cd build && cmake .. && cmake --build . && ctest
+```
+
+If you'd like to see log messages for individual test cases, logging has to be enabled and
+the test has to be executed alone in it's own executable like this:
+
+```bash
+export BOOST_TEST_LOG_LEVEL=message
+$ cd build && ./testBin/test_executable
 ```
--- tests/command_test.cpp
+++ tests/command_test.cpp
@@ -1,22 +1,19 @@
-// #define BOOST_TEST_DYN_LINK
-// #define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE commands
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE "commands"
 #include <boost/test/unit_test.hpp>
 #include "../cpp-commander/src/CommandRegistry.hpp"
 #include "../furnace/commands/CreateCommand.hpp"
 
 BOOST_AUTO_TEST_CASE( registry )
 {
+    BOOST_TEST_MESSAGE("Running none testing test for now.");
     int i = 2;
     const char *a[] = {
         (char*)("furnace"),
         (char*)("create")
     };
     Commander::CommandRegistry* registry = new Commander::CommandRegistry(i, a);
-    BOOST_TEST_MESSAGE ("ANYAD");
-    BOOST_CHECK(true);
-    // registry->Register(&(Commands::NewCreateCommand));
-    // registry->Dump();
-    // BOOST_CHECK_EQUAL(, 1);
+    registry->Register(&(Commands::NewCreateCommand));
+    // If this would be a nullptr, the test would fail.
+    BOOST_CHECK(registry);
 }
