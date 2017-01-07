bd86b1741b6bb63f83e79e7c92b09f62ecf95ba2
Gergely Brautigam <skarlso777@gmail.com>
2017-01-07 12:19:20 +0100

    Tests are still not compiling.

--- tests/command_test.cpp
+++ tests/command_test.cpp
@@ -1,16 +1,21 @@
-#define BOOST_AUTO_TEST_MAIN
-#include <boost/test/auto_unit_test.hpp>
+// #define BOOST_TEST_MAIN
+// #define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE "commands"
+#include <boost/test/unit_test.hpp>
 #include "../cpp-commander/src/CommandRegistry.hpp"
 #include "../furnace/commands/CreateCommand.hpp"
 
 BOOST_AUTO_TEST_CASE( registry )
 {
-    const int argc = 1;
-    const char* argv[] = {
+    int i = 2;
+    const char *a[] = {
         (char*)("furnace"),
         (char*)("create")
     };
-    Commander::CommandRegistry* registry = new Commander::CommandRegistry(argc, argv);
+    Commander::CommandRegistry* registry = new Commander::CommandRegistry(i, a);
+    BOOST_TEST_MESSAGE ("ANYAD");
+    BOOST_CHECK(true);
     // registry->Register(&(Commands::NewCreateCommand));
     // registry->Dump();
     // BOOST_CHECK_EQUAL(, 1);
