4b163482c725ccefa11165fd9cd28e550d052086
Gergely Brautigam <skarlso777@gmail.com>
2017-01-08 10:14:47 +0100

    Added AWS SDK support core first. Let's see what travis makes of this.

--- furnace/furnace.cpp
+++ furnace/furnace.cpp
@@ -1,11 +1,15 @@
 #include "../cpp-commander/src/CommandRegistry.hpp"
 #include "commands/CreateCommand.hpp"
+#include <aws/core/Aws.h>
 
 int main(int argc, const char *argv[])
 {
+    Aws::SDKOptions options;
+    Aws::InitAPI(options);
     Commander::CommandRegistry* registry = new Commander::CommandRegistry(argc, argv);
     registry->Register(&(Commands::NewCreateCommand));
     registry->Dump();
     registry->Execute();
+    Aws::ShutdownAPI(options);
     return 0;
 }
