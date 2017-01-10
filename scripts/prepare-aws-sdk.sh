a365d6190c5d253742c2454ae9cfa8bdc7884e08
Balazs Nadasdi <yitsushi@gmail.com>
2017-01-10 14:39:47 +0100

    Update cpp-commander and add a bundler like dependency prebuild with aws-sdk (#3)

---
+++ scripts/prepare-aws-sdk.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+LIBS_WE_NEED="s3;cloudformation"
+
+if [ ! -d aws-sdk-cpp ]; then
+  echo "AWS SDK Source: Not found"
+  git clone --depth 1 https://github.com/aws/aws-sdk-cpp aws-sdk-cpp
+else
+  echo "AWS SDK Source: Found"
+fi
+
+pushd aws-sdk-cpp
+
+if [ ! -d build ]; then mkdir build; fi
+
+pushd build
+
+cmake \
+  -DBUILD_ONLY="${LIBS_WE_NEED}" \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DENABLE_TESTING=OFF \
+  -Wno-dev \
+  ..
+
+make
+
