0b727c5d62b356fcc403c7c29dd9ae0ce7dc1f6d
Gergely Brautigam <skarlso777@gmail.com>
2017-01-24 15:47:52 +0100

    Added proper library handling, and stack yml.

--- furnace/commands/CreateCommand.cpp
+++ furnace/commands/CreateCommand.cpp
@@ -3,6 +3,8 @@
 #include <Commander/ArgumentRegistry.hpp>
 #include <Commander/CommandDescriptor.hpp>
 #include "CreateCommand.hpp"
+//#include <aws/cloudformation/CloudFormationClient.h>
+#include <aws/cloudformation/CloudFormationClient.h>
 
 namespace Commands {
   void CreateCommand::Execute(Commander::ArgumentRegistry* args)
@@ -12,6 +14,7 @@ namespace Commands {
     if (args->FindByKey("verbose") != nullptr) {
       std::cout << "Verbose mode is active..." << std::endl;
     }
+    Aws::CloudFormation::CloudFormationClient cfClient;
   }
 
   Commander::CommandDescriptor* NewCreateCommand(const std::string appName)
